34a7666eb0511c5470f988459b3b50e6a513c2cd
Stuart Dunkeld <stuartd@gmail.com>
2021-04-17 20:06:32 +0100

    new sorting and pics

--- shared/pics.js
+++ shared/pics.js
@@ -1,36 +1,49 @@
 function shuffle (items) {
-  var i = 0, j = 0, temp = null
+  let i = 0, j = 0, temp = null;
 
-  for (i = items - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = items[i]
-    items[i] = items[j]
-    items[j] = temp
+  for (i = items.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = items[i];
+    items[i] = items[j];
+    items[j] = temp;
   }
+
+  return items;
 }
-rm
-shuffleAndStore(imageCount, tag) {
-	var pics = new Array();
-	for (let i = 0; i <= imageCount; i++) {
+
+function shuffleAndStore(imageCount, tag) {
+	let pics = new Array();
+	for (let i = 0; i < imageCount; i++) {
   		pics.push(i);
 	}
 
 	pics = shuffle(pics);
 
-	sessionStorage.setItem(tag, pics);
+	sessionStorage.setItem(tag, JSON.stringify(pics));
 
 	return pics;	
 }
 
-getImageIndex(imageCount, tag) {
-	var pictureIndexes = sessionStorage.getItem(tag);
+function getImageIndex(imageCount, tag) {
 
-	if (pictureIndexes === null || pictureIndexes.length === 0) {
-	   pictureIndexes = shuffleAndStore(imageCount, tag);
+	// OK, new plan, as the items have to be stored as strings.
+	// If this is the first run or we've run out of items, create a shuffled array and store it as Json.
+
+	let pictureIndexes = null;
+	let pictureIndexData = sessionStorage.getItem(tag);
+	if (pictureIndexData == null || pictureIndexData == "[]") {
+		pictureIndexes = shuffleAndStore(imageCount, tag);
+		console.log("Shuffled images: " + pictureIndexes)
 	}
-	
-	var index = pictureIndexes[0];
+	else {
+		pictureIndexes = JSON.parse(pictureIndexData);
+	}
+
+	// images are zero based
+	let index = pictureIndexes[0];
 	pictureIndexes = pictureIndexes.slice(1);
-	sessionStorage.setItem(tag, pictureIndexes);
+	sessionStorage.setItem(tag, JSON.stringify(pictureIndexes));
+
+	return index;
 }
 
